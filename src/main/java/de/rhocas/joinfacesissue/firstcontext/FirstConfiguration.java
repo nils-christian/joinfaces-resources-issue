a5cd650d04914dbd2f634a71278dd8916c57a13c
Lars Grefer <github@larsgrefer.de>
2019-05-28 01:55:19 +0200

    PoC for resource moving

--- src/main/java/de/rhocas/joinfacesissue/firstcontext/FirstConfiguration.java
+++ src/main/java/de/rhocas/joinfacesissue/firstcontext/FirstConfiguration.java
@@ -1,5 +1,11 @@
 package de.rhocas.joinfacesissue.firstcontext;
 
+import de.rhocas.joinfacesissue.PrefixWebResourceRoot;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.webresources.StandardRoot;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
@@ -7,8 +13,15 @@ import org.springframework.context.annotation.PropertySource;
 import de.rhocas.joinfacesissue.shared.SharedConfiguration;
 
 @Configuration
-@Import ( SharedConfiguration.class )
-@PropertySource ( "classpath:firstcontext/application.properties" )
+@Import(SharedConfiguration.class)
+@PropertySource("classpath:firstcontext/application.properties")
 public class FirstConfiguration {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer() {
+        return factory -> factory.addContextCustomizers(context -> {
+            context.setResources(new PrefixWebResourceRoot("/a"));
+        });
+    }
 
 }
